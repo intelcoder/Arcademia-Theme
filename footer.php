b4d2cfc8b8e8ff880399cd10fe7535856e25a5f4
intelcoder <seongjunkim7162@gmail.com>
2018-09-17 22:23:51 -0400

    mobile footer style added

--- footer.php
+++ footer.php
@@ -10,17 +10,16 @@
   $location = $query -> posts[0];
 ?>
 
- <div class="footer main-body-container row no-margin no-padding">
-   <div class="col-xs-12 col-sm-4 col-md-4">
+ <div class="footer main-body-container row no-margin">
+   <div class="col-xs-12 col-sm-4 col-md-4 location">
       <div class="footer-office"><?php echo($location -> post_title )?></div>
       <?php include(locate_template('template-parts/address.php')); ?>
    </div>
    <div class="logos-container col-xs-12 col-sm-4 col-md-4 center-md center-sm middle-md middle-sm">
      logo shit
    </div>
-   <div class="col-xs-12 col-sm-4 col-md-4 end-sm end-md no-padding">
-   <?php wp_nav_menu();?>
-
+   <div class="footer-menu col-xs-12 col-sm-4 col-md-4 end-sm end-md no-padding">
+    <?php wp_nav_menu();?>
    </div>
    <div class="full-width reserve">© Arcademia Group Inc. 2018. All rights reserved.</div>
  </div>
